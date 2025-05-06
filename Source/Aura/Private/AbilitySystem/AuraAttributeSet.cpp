852f410d3ce2e6f189ad0bc54446ecba6dc8862b
Neurolancer81 <87434607+Neurolancer81@users.noreply.github.com>
2025-05-06 14:46:45 -0400

    Added first gameplay effects

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -3,13 +3,16 @@
 
 #include "AbilitySystem/AuraAttributeSet.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "GameplayEffectExtension.h"
+#include "GameFramework/Character.h"
 #include "Net/UnrealNetwork.h"
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
 	InitHealth(50.0f);
 	InitMaxHealth(100.0f);
-	InitMana(50.0f);
+	InitMana(20.0f);
 	InitMaxMana(50.0f);
 }
 
@@ -21,6 +24,67 @@ void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& Ou
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+	
+}
+
+void UAuraAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp<float>(NewValue, 0.0f, GetMaxHealth());
+	}
+
+	if (Attribute == GetManaAttribute())
+	{
+		NewValue = FMath::Clamp<float>(NewValue, 0.0f, GetMaxMana());
+	}
+}
+
+void UAuraAttributeSet::SetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& EffectPropertiesOut) const
+{
+	EffectPropertiesOut.EffectContextHandle = Data.EffectSpec.GetContext();
+	EffectPropertiesOut.SourceASC = EffectPropertiesOut.EffectContextHandle.GetOriginalInstigatorAbilitySystemComponent();
+
+	if (IsValid(EffectPropertiesOut.SourceASC) &&
+		EffectPropertiesOut.SourceASC->AbilityActorInfo.IsValid() &&
+		EffectPropertiesOut.SourceASC->AbilityActorInfo->AvatarActor.IsValid())
+	{
+		EffectPropertiesOut.SourceAvatarActor = EffectPropertiesOut.SourceASC->GetAvatarActor();
+		EffectPropertiesOut.SourceController = EffectPropertiesOut.SourceASC->AbilityActorInfo->PlayerController.Get();
+		if (EffectPropertiesOut.SourceController == nullptr && EffectPropertiesOut.SourceController != nullptr)
+		{
+			if (const APawn* Pawn = Cast<APawn>(EffectPropertiesOut.SourceAvatarActor))
+			{
+				EffectPropertiesOut.SourceController = Pawn->GetController();
+			}
+		}
+		if (EffectPropertiesOut.SourceController != nullptr)
+		{
+			EffectPropertiesOut.SourceCharacter = Cast<ACharacter>(EffectPropertiesOut.SourceController->GetPawn());
+		}
+	}
+
+	if (Data.Target.AbilityActorInfo.IsValid() &&
+		Data.Target.AbilityActorInfo->AvatarActor.IsValid())
+	{
+		EffectPropertiesOut.TargetAvatarActor = Data.Target.AbilityActorInfo->AvatarActor.Get();
+		EffectPropertiesOut.TargetController = Data.Target.AbilityActorInfo->PlayerController.Get();
+		EffectPropertiesOut.TargetCharacter = Cast<ACharacter>(EffectPropertiesOut.TargetAvatarActor);
+		EffectPropertiesOut.TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(EffectPropertiesOut.TargetAvatarActor);
+	}
+	
+}
+
+
+void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	FEffectProperties EffectProperties;
+	SetEffectProperties(Data, EffectProperties);
+	
 	
 }
 
@@ -43,3 +107,4 @@ void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) 
 {
 	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxMana, OldMaxMana);
 }
+
