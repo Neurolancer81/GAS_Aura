9a7a6eb2d5e846d8e7292e5cce530dd71ece586b
Neurolancer81 <87434607+Neurolancer81@users.noreply.github.com>
2025-08-11 11:37:48 -0400

    Added ASC and AS

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,7 +3,10 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "AbilitySystemComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Net/UnrealNetwork.h"
+#include "Player/AuraPlayerState.h"
 
 
 AAuraCharacter::AAuraCharacter()
@@ -19,3 +22,32 @@ AAuraCharacter::AAuraCharacter()
 
 	
 }
+
+void AAuraCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	// Init ability actor for the server
+	InitAbilityActorInfo();
+	
+}
+
+void AAuraCharacter::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+
+	// Init ability actor for the client
+	InitAbilityActorInfo();
+	
+}
+
+void AAuraCharacter::InitAbilityActorInfo()
+{
+	AAuraPlayerState* AuraPlayerState = GetPlayerState<AAuraPlayerState>();
+	check(AuraPlayerState)
+
+	AuraPlayerState->GetAbilitySystemComponent()->InitAbilityActorInfo(AuraPlayerState, this);
+	AbilitySystemComponent = AuraPlayerState->GetAbilitySystemComponent();
+	AttributeSet = AuraPlayerState->GetAttributeSet();
+}
+
